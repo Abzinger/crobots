abb516661b94a5a186f736a8a4ecf7a455f368c6
Suido <suivalli@gmail.com>
2016-02-26 11:02:26 +0200

    Changed Simulation code, added robots

--- Simulation/public/simulation.js
+++ Simulation/public/simulation.js
@@ -1,4 +1,8 @@
-var simulation = new Phaser.Game(1600, 800, Phaser.AUTP, 'simulation', { preload: preload, create: create, update: update });
+var simulation = new Phaser.Game(1600, 800, Phaser.AUTO, 'simulation', { preload: preload, create: create, update: update });
+
+var GRID_WIDTH  = 100;
+var GRID_HEIGHT = 200;
+var VELOCITY = 30;
 
 var isOver;
 
@@ -14,16 +18,21 @@ function preload() {
 	simulation.load.image('car5', './public/assets/cars/car5_200.png');
 	simulation.load.image('car6', './public/assets/cars/car6_200.png');
 	
+	simulation.load.image('robot', './public/assets/robots/robot_200.png');
+	
 }
 
 function create() {
-	
+		
 	createParkingLot();
+	
+	r = simulation.add.sprite(150,300,'robot');
+	r.anchor.setTo(0.5,0.5);
+	r.inputEnabled = true;
 	
 	s = simulation.add.sprite(150,300,'car1');
 	s.anchor.setTo(0.5,0.5);
 	s.inputEnabled = true;
-	
 	
 	
 	t = simulation.add.sprite(250,300,'car2');
@@ -35,6 +44,8 @@ function create() {
 	simulation.add.sprite(1000,600,'car4');
 	simulation.add.sprite(500,400,'car5');
 	simulation.add.sprite(300,0,'car6');
+	
+	
 }
 
 function createParkingLot() {
@@ -46,17 +57,45 @@ function createParkingLot() {
 	}
 }
 
-function update() {
-	
-	simulation.physics.enable(s, Phaser.Physics.ARCADE);
-	
-	isOver = s.input.pointerOver()? true : false;
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
 
-	if (simulation.input.activePointer.leftButton.isDown){
-		if (isOver){
-			moveAround();
-		}
+function moveTo(object, direction, spaces){
+	var startX = object.x;
+	var startY = object.y;
+	
+	if (direction == 'E'){		
+			
+	} else if (direction == 'W'){
+		simulation.physics.arcade.moveToXY(object,startX - (GRID_WIDTH * spaces), startY, VELOCITY);
+			
+	} else if (direction == 'N'){
+		simulation.physics.arcade.moveToXY(object,startX, startY + (GRID_HEIGHT * spaces), VELOCITY);
+			
+	} else if (direction == 'S'){
+		simulation.physics.arcade.moveToXY(object,startX, startY -(GRID_HEIGHT * spaces), VELOCITY);
+		
 	}
+
+	
+};
+
+function update() {	
+	isOver = s.input.pointerOver()? true : false;
+	
+	if (isOver){
+		simulation.input.onDown.add(moveAround,this);
+	}
+	
+	s.body.velocity.x = 0;
+	s.body.velocity.y = 0;
+	s.body.angularVelocity = 0;
 }
 
 function getInstructions(e){
@@ -68,11 +107,6 @@ function getInstructions(e){
 }
 
 function moveAround(){	
-	tween = simulation.add.tween(s).to({ x: [50,50,50,650,650], y:[300, 500, 700,700,500]}, 5000, 'Linear', false, 0);
-	tween2 = simulation.add.tween(t).to({ x: [150], y:[300]}, 100, 'Linear', false, 0);
-	
-	tween.chain(tween2);
-		
-	tween.start();
+	moveTo(s, 'N', 1);
 
 }
