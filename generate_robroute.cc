9dd714af1a841ff2bcb20d622d263cf7c1725757
Dirk Oliver Theis <dotheisatutdotee@gmail.com>
2016-05-11 07:31:20 -0400

    added robroute problem file generator

--- generate_robroute.cc
+++ generate_robroute.cc
@@ -4,7 +4,9 @@
 
 #include "example_grids.hh"
 #include "grid.hh"
+#include "grid_properties.hh"
 #include "grid_stat.hh"
+#include "robroute.hh"
 
 #include <iostream>
 #include <fstream>
@@ -25,16 +27,25 @@ int main(int argc, const char *argv[]) try
 
     const char * const usage_msg =
         "USAGE: \n"
-        "generate_robroute -rnd filename NS EW p r c0 c1 c2\n"
+        "\n"
+        "generate_robroute -rnd NS EW p          out_filename r c0 c1 c2\n"
         "  --random parking lot of given dimensions\n"
-        "    with random initial + terminal configurations;\n"
-        "generate_robroute -lib parking_lot_name filename r c0 c1 c2\n"
-        "  --use library parking lot of that name\n"
-        "    with random initial + terminal configurations\n"
+        "    with edge (no-wall) probability p;\n"
+        "    random initial + terminal configurations;\n"
+        "\n"
+        "generate_robroute -lib parking_lot_name out_filename r c0 c1 c2\n"
+        "  --use library parking lot of that name;\n"
+        "    random initial + terminal configurations\n"
+        "\n"
         "generate_robroute -lib\n"
-        "  --print list of library parking lots to stdout."
+        "  --print list of library parking lots to stdout.\n"
+        "\n"
+        "generate_robroute -file in_filename     out_filename r c0 c1 c2\n"
+        "  --take the parking lot from in_filename;\n"
+        "    add random initial + terminal configurations\n"
+        "\n"
         "Make sure that r, c0+c1+c2  <  free slots.\n"
-        "Data is writte to 'filename.robroute'.\n";
+        "File 'out_filename' is truncated before writing.\n";
 
     std::cout<<"Robroute Generator --- Part of the Robot Routing project\n"<<std::endl;
     if (argc<7 && argc!=2) {
@@ -54,18 +65,23 @@ int main(int argc, const char *argv[]) try
         }
         fillarg_begin = 5;
         {// check args
-            const int    NS        = std::atoi(argv[1]);
-            const int    EW        = std::atoi(argv[2]);
-            const double p         = std::atof(argv[3]);
+            const int    NS        = std::atoi(argv[2]);
+            const int    EW        = std::atoi(argv[3]);
+            const double p         = std::atof(argv[4]);
             if (NS<1 || NS>255)   return std::cerr<<"NS must be in {1,...,255}\n",2;
             if (EW<1 || EW>255)   return std::cerr<<"EW must be in {1,...,255}\n",2;
-            if (p<0.  || p>1.)    return std::cerr<<"p must be in [0,1]\n",2;
+            if (p<=0.  || p>1.)   return std::cerr<<"p must be in ]0,1]\n",2;
         }
 
-        const unsigned short NS  = std::atoi(argv[1]);
-        const unsigned short EW  = std::atoi(argv[2]);
-        const double         p   = std::atof(argv[3]);
-        p_grid = new RndGrid(NS,EW,p);
+        const unsigned short NS  = std::atoi(argv[2]);
+        const unsigned short EW  = std::atoi(argv[3]);
+        const double         p   = std::atof(argv[4]);
+        std::printf("Searching connected(!) random grid with NS %d, EW %d, and edge probability %f",NS,EW,p);
+        do {
+            p_grid = new RndGrid(NS,EW,p);
+            std::printf(".");
+        } while (!( GridSpace::grid_fullsize(*p_grid) && GridSpace::grid_connected(*p_grid) ));
+        std::printf(" Found!\n");
         std::printf("Using random grid: NS=%d; EW=%d; slots=%d.\n",p_grid->NS_sz(),p_grid->EW_sz(),p_grid->numo_slots());
     } else if (string(argv[1]) == "-lib") {  // example grid
         const bool print_only = ( argc==2 );
@@ -85,6 +101,20 @@ int main(int argc, const char *argv[]) try
         if (print_only || string(argv[2]) == "Marsi3E") { p_grid = new GridSpace::Marsi3E_ExampleGrid; std::printf(" Marsi3E grid: NS=%d; EW=%d; slots=%d.\n",p_grid->NS_sz(),p_grid->EW_sz(),p_grid->numo_slots()); }
 
         if (print_only) return 1;
+    } else if (string(argv[1]) == "-file") {
+        if (argc!=8) {
+            std::cerr<<usage_msg;
+            return 30;
+        }
+        fillarg_begin = 3;
+
+        const char * const in_filename = argv[1];
+        std::ifstream in_file {in_filename};
+        in_file.exceptions(in_file.exceptions() | std::ios_base::badbit | std::ios_base::failbit);
+        std::string                                comments;
+        std::vector< GridSpace::Stat_Vector_t >    fullsol;
+        p_grid = GridSpace::read_robroute(in_file, &fullsol, &comments);
+        std::printf("Using grid from file %s: NS=%d; EW=%d; slots=%d.\n",in_filename,p_grid->NS_sz(),p_grid->EW_sz(),p_grid->numo_slots());
     } else { // error
         std::cerr<<usage_msg;
         return 2;
@@ -107,7 +137,7 @@ int main(int argc, const char *argv[]) try
         if (n_C2>=(int)G.numo_slots())           return std::cerr<<"Too few free slots ("<<G.numo_slots()<<") for this number of type-2 cars ("<<n_C2<<")\n",32;
         if (n_C0+n_C1+n_C2>=(int)G.numo_slots()) return std::cerr<<"Too few free slots ("<<G.numo_slots()<<") for this number of cars ("<<n_C0+n_C1+n_C2<<")\n",32;
     }
-    const string   filename  = string(argv[fillarg_begin+0])+".robroute";
+    const string   filename  = string(argv[fillarg_begin+0]);
     const unsigned g_size    = G.numo_slots();
     const unsigned n_robots  = std::atoi(argv[fillarg_begin+1]);
     const unsigned n_C0      = std::atoi(argv[fillarg_begin+2]);
@@ -126,23 +156,13 @@ int main(int argc, const char *argv[]) try
     Rnd_Grid_Stat gState_0 { G, {n_empty,n_C0,n_C1,n_C2}, n_robots };
     Rnd_Grid_Stat gState_1 { G, {n_empty,n_C0,n_C1,n_C2}, n_robots };
 
-    string whole_file =
-        "# robroute problem file\n"
-        "# Syntax:\n"
-        "# NS EW \\ngrid(raw)\\ntmax \\ngridstat_0(raw)\\n ... \\ngridstat_tmax(raw)\\n\n"
-        "# Comments (lines like this one) are only allowed as a preamble.\n";
-    whole_file
-        += std::to_string(G.NS) + " " + std::to_string(G.EW)+"\n"
-        + G.raw_write() + "\n"
-        + std::to_string(1) + "\n";
-    whole_file += GridSpace::raw_write( gState_0 ) + '\n';
-    whole_file += GridSpace::raw_write( gState_1 ) + '\n';
-
     std::cout<<"Writing file ``"<<filename<<"'' ...";
     std::ofstream file {filename};
     file.exceptions(file.exceptions() | std::ios_base::badbit);
-    file<<whole_file<<std::flush;
+    std::vector< GridSpace::Stat_Vector_t > statvec { gState_0, gState_1 };
+    GridSpace::write_robroute(file,statvec);
     file.close();
+
     delete p_grid;
 
     std::cout<<"Success."<<std::endl;
