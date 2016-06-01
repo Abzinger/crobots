64f3b1c4fdac7104440e2b24753b06410315b64f
Suido <suivalli@gmail.com>
2016-06-01 20:49:08 +0300

    Fixing casing bug no.2

--- Simulation/tests.py
+++ Simulation/tests.py
@@ -8,7 +8,7 @@ class TestRouteList(unittest.TestCase):
         self.assertIsInstance(json.loads(fr.get_json_route_list()), list)
 
     def test_holds_nine_scenarios(self):
-        self.assertEqual(len(json.loads(fr.get_json_route_list())), 9)
+        self.assertEqual(len(json.loads(fr.get_json_route_list())), 38)
 
     def test_first_name_Scenario_1(self):
         self.assertEqual(json.loads(fr.get_json_route_list())[0]["name"], "Scenario 1")
